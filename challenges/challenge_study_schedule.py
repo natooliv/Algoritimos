f11b78e2bef2c36ec5e055a44946ad057fd27850
Natalia de Oliveira <natoliveira7300hotmail.com>
2023-07-10 14:53:17 -0300

    Requisito 01 e 02

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    # Verifica se target_time é None. Se for, significa que não há um tempo-alvo definido, e a função retorna None. Isso é útil para lidar com casos em que o tempo-alvo não está especificado.
+
+    alunos = 0
+    for periodo in permanence_period:
+        if not isinstance(periodo[0], int) or not isinstance(periodo[1], int):
+            return None
+        if periodo[0] <= target_time <= periodo[1]:
+            alunos += 1
+    return alunos
